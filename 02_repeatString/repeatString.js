38176fc420e679f412bb3374e61bf6b96c854882
rac640 <50179707+rac640@users.noreply.github.com>
2024-08-21 11:37:59 -0400

    finished reverse string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
+// copied from the solution to test out the npm test. COME BACK TO THIS AND DO IT AGAIN. 
+const repeatString = function (word, times) {
+    if (times < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+  };
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,43 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const myArray = string.split("");
 
-};
+    // Step 2: Start at the end of the Array
+    
+    // This shows the length of the array, which I will use to start at the last element of the array.  
+    let lastLetterInt = myArray.length;
+    
+    
+    
+    // Step 3:  Iterate starting from the last to the first.  
+    
+    
+    // Maybe use a global undefined variable and store the values
+    let reversedArray = []; 
+    
+    for (let i = lastLetterInt;  i >= 0; i--){
+        // this test seems to work fine, since it logs the text "olleh line-by-line"
+        // console.log(text[i]);
+    
+        // HERE IS WHERE I'M FACING PROBLEMS. Basically, I want to create a new array from the new outputs
+        // I want to output [o,l,l,e,h] so that I can convert it to a string again. 
+       reversedArray.push(myArray[i]);
+       
+    //  I finally figured it out after 2 days! Inspired by:  https://stackoverflow.com/questions/12491101/javascript-create-array-from-for-loop
+    }
+    
+    let finalArray = reversedArray.join('');
+
+    // Make sure to return the final output! 
+    return finalArray;     
+    };
+
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
