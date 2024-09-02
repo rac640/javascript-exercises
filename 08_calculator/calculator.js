e3f0f8eda3bb8c4fbb80cf079116e3998b4be1e7
rac640 <50179707+rac640@users.noreply.github.com>
2024-09-02 10:44:35 -0400

    completed exercise 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,83 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+	addedNumbers = numOne + numTwo; 
+  return addedNumbers;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	subtractNumbers = numOne - numTwo;
+  return subtractNumbers;
 };
 
-const sum = function() {
-	
+const sum = function(arrayNumbers) {
+  
+  if (arrayNumbers.length === 1){
+    return parseInt(arrayNumbers);
+  }
+  
+  else{
+
+  
+  let newArray = [];
+
+  for (let i = arrayNumbers.length -1; i >= 0; i--){
+
+      newArray.push(arrayNumbers[i]);
+  }
+
+  let sum = 0;
+
+  for (i = newArray.length -1 ; i >= 0 ; i-- ){
+      sum = sum+newArray[i];
+  }
+  return sum;
+
+}
+
 };
 
-const multiply = function() {
+const multiply = function(arrayToMultiply) {
+  if (arrayToMultiply.length === 1){
+    return parseInt(arrayToMultiply);
+  }
+  else {
+    let newArray = [];
+    for (let i = arrayToMultiply.length -1; i >= 0; i--){
+      newArray.push(arrayToMultiply[i]);
+    }
+
+    let multiply = 1;
+
+    for (i = newArray.length -1; i >= 0; i -- ){
+      multiply = multiply * arrayToMultiply[i];
+    }
+
+    return multiply;
+
+  }
+  
+
 
 };
 
-const power = function() {
-	
+const power = function(baseNumber, exponentNumber) {
+	return baseNumber**exponentNumber;
 };
 
-const factorial = function() {
-	
+const factorial = function(startNumber) {
+	if (startNumber === 0 || startNumber === 1){
+    return 1;
+  }
+  else if (startNumber === 2){
+    return 2;
+  }
+  else {
+    let multiply = 1;
+    for (i = startNumber; i >0; i--){
+      multiply = multiply * i;
+    }
+    return multiply;
+
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+// make the string lowercase, remove the spaces, and remove all the special characters.
+
+let editedString = str.toLowerCase().replace(/ /g,"").replace(/[#!,.]/g,""); 
+
+
+// step 1: make it into an array 
+
+const originalArray = Array.from(editedString);
+
+// Step 2: make a new array (Shows reverse of originalArray, and compares it)
+const newArray = [];
+
+for (let i = originalArray.length-1; i >=0; i--){
+    newArray.push(originalArray[i]);
+}
+
+
+// step 3: compare newArray with originalArray: https://www.freecodecamp.org/news/how-to-compare-arrays-in-javascript/ 
+if (newArray.toString() === originalArray.toString()) return true;
+else return false;
 
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
